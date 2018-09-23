49ecc152cf7f234bfbff8651ee0ac30d985539ca
Stefan Holecko <stevoh096@gmail.com>
2018-09-23 22:44:46 +0200

    hello world

---
+++ src/test/java/cz/muni/fi/pa165/hello_world/AppTest.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.pa165.hello_world;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
